75f2fe13b475556dc2603bfea2a4cab398021476
rhgrive3 <rhgrive@gmail.com>
2026-08-24 07:12:42 +0900

    fix(navigation): respect zero history limit on reset

--- js/navigation.js
+++ js/navigation.js
@@ -16,7 +16,7 @@ export class NavigationHistory {
   }
 
   reset(entry) {
-    this.entries = entry ? [entry] : [];
+    this.entries = entry && this.limit > 0 ? [entry] : [];
     this.index = this.entries.length - 1;
     this.onChange(this.snapshot());
   }
